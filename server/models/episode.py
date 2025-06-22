f71e97bdc9b95c658393f9cafd261fe55e195f87
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-22 22:57:53 +0300

    episode update

--- server/models/episode.py
+++ server/models/episode.py
@@ -3,3 +3,4 @@ from server.app import db
 class Episode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
