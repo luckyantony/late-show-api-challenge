2aa25777488e2b5481130d338a16b6bd8a57540c
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-22 22:56:05 +0300

    user file update

---
+++ server/models/user.py
@@ -0,0 +1,6 @@
+from server.app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
