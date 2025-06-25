98afd9e84548e6e5a8a555f7e61015f75e5f8573
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-25 12:37:53 +0300

    Updated episodes file

--- server/models/episode.py
+++ server/models/episode.py
@@ -4,4 +4,5 @@ class Episode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String, nullable=False)
     number = db.Column(db.Integer, nullable=False)
-    
+
+    appearances = db.relationship("Appearance", backref="episode", cascade="all, delete")
