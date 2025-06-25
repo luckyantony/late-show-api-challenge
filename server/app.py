99a86f559c0ed28c9a99b0e843d002aa5244e8fa
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-25 12:54:22 +0300

    Configuring Postgre sql

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
--- server/config.py
+++ server/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
-        "postgresql://postgres:yourpassword@localhost:5432/late_show_db"
+        "postgresql://postgres:luckyantony@localhost:5432/late_show_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "supersecretkey"
