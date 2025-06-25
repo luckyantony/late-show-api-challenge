39abbb439442b2e59beed73b78b7469f07fd1283
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-25 12:35:02 +0300

    Updated config file

--- server/config.py
+++ server/config.py
@@ -2,5 +2,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-SQLALCHEMY_DATABASE_URI = "postgresql://<user>:<password>@localhost:5432/late_show_db"
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
+        "postgresql://postgres:yourpassword@localhost:5432/late_show_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "supersecretkey"
