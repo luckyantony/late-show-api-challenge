183baa6a0fab322ecff612d72c86ce1e21a887e0
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-25 12:37:16 +0300

    updated guests file

--- server/models/guest.py
+++ server/models/guest.py
@@ -4,4 +4,5 @@ class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     occupation = db.Column(db.String, nullable=False)
-    
+
+    appearances = db.relationship("Appearance", backref="guest", cascade="all, delete")
